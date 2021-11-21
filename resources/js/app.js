cd8ed424452ecd3c95adc2593ad2341765192940
DreamUpstream <gabrielius1621@gmail.com>
2021-11-21 23:18:59 +0200

    v1

---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+window.axios = require('axios');
+window.bootstrap = require('bootstrap');
+
+import './places.js';
+import './geoCityFinder';
+import './majorCitiesRender';
---
+++ services/MeteoApi.js
@@ -0,0 +1,34 @@
+const request = require('request');
+
+module.exports.getPlaces = function() {
+	return new Promise(function(resolve) {
+		request.get('https://api.meteo.lt/v1/places').then(response => {
+			let places = JSON.parse(response.body);
+			resolve(places);
+		});
+	});
+}
+
+module.exports.getPlaceForecasts = function(placeCode) {
+	return new Promise(function(resolve) {
+		request.get(`https://api.meteo.lt/v1/places/${placeCode}/forecasts/long-term`).then(response => {
+			let places = JSON.parse(response.body);
+			resolve(places);
+		});
+	});
+}
+
+
+module.exports.getTown = function(Coordinates) {
+	let coordinatesv2 = Coordinates.replaceAll("-",".");
+	coordinatesv2 = coordinatesv2.split(("_"));
+	let lat = coordinatesv2[0];
+	let lng = coordinatesv2[1];
+	let gettext = "https://us1.locationiq.com/v1/reverse.php?key=pk.7e9ff657e0ebf4587f8e9d0072b456cb&lat=" + lat + "&lon=" + lng + "&format=json";
+	return new Promise(function(resolve) {
+		request.get(gettext).then(response => {
+			let locationInfos = JSON.parse(response.body);
+			resolve(locationInfos.address.town);
+		});
+	});
+}
